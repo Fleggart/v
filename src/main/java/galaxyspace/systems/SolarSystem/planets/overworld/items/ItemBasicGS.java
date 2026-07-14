da7561962882965b959355871567b646ce652a5d
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-15 02:12:49 +0900

    Add files via upload

--- src/main/java/galaxyspace/systems/SolarSystem/planets/overworld/items/ItemBasicGS.java
+++ src/main/java/galaxyspace/systems/SolarSystem/planets/overworld/items/ItemBasicGS.java
@@ -524,7 +524,7 @@ public class ItemBasicGS extends Item implements ISortableItem{
 
 	public static Object[] getColonistKitRecipe() {
 		ItemStack[] stacks = new ItemStack[] {
-				new ItemStack(GCBlocks.oxygenDistributor, 1), new ItemStack(GCBlocks.oxygenCollector, 1), new ItemStack(GCBlocks.oxygenCompressor, 1),
+				new ItemStack(GCBlocks.oxygenCollector, 1), new ItemStack(GCBlocks.oxygenCompressor, 1),
 				new ItemStack(GCBlocks.solarPanel, 1, 4), new ItemStack(GSItems.BASIC, 1, 20), new ItemStack(GCBlocks.solarPanel, 1, 4),
 				new ItemStack(GCBlocks.machineTiered, 1, 0), new ItemStack(MarsBlocks.machine, 1, 0), new ItemStack(GCBlocks.machineBase2, 1, 8)
 		};
